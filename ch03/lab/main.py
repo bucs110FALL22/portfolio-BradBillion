dec7dfd180b9f9a69308b41e2bb1c3053f98aa1f
Bradley Vaval <90658099+BradBillion@users.noreply.github.com>
2022-09-23 00:53:59 +0000

    turtle racer

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,26 +1,146 @@
-import turtle #1. import modules
+import turtle  #1. import modules
 import random
+import time
+import pygame
+import math
 
 #Part A
-window = turtle.Screen() # 2.  Create a screen
+window = turtle.Screen()  # 2.  Create a screen
 window.bgcolor('lightblue')
 
-michelangelo = turtle.Turtle() # 3.  Create two turtles
-leonardo = turtle.Turtle()
-michelangelo.color('orange')
-leonardo.color('blue')
-michelangelo.shape('turtle')
-leonardo.shape('turtle')
+turtA = turtle.Turtle()  # 3.  Create two turtles
+turtB = turtle.Turtle()
+turtC = turtle.Turtle()
+turtA.color('red')
+turtB.color('blue')
+turtC.color('black')
+turtA.shape('turtle')
+turtB.shape('turtle')
+turtC.shape('turtle')
 
-michelangelo.up() # 4. Pick up the pen so we don’t get lines
-leonardo.up()
-michelangelo.goto(-100,20)
-leonardo.goto(-100,-20)
-
+turtA.up()  # 4. Pick up the pen so we don’t get lines
+turtB.up()
+turtC.up()
+turtA.goto(-100, 20)
+turtB.goto(-100, -20)
+turtC.goto(-80, 40)
+turtC.down()
+turtC.goto(-80, -40)
+turtC.up()
+turtC.goto(80, 40)
+turtC.down()
+turtC.goto(80, -40)
+turtC.up()
+turtC.goto(100, 0)
+turtC.left(180)
 ## 5. Your PART A code goes here
+print("")
+race1 = True
+race2 = False
+print("Starting Race 1")
+turtA.down()
+turtB.down()
+for i in range(4):
+    if i == 3:
+        print("Go!")
+    else:
+        print(3 - i)
+        time.sleep(1)
+turtA.forward(random.randrange(1, 201))
+turtB.forward(random.randrange(1, 201))
+time.sleep(1)
+if turtA.xcor() > turtB.xcor():
+    print("Red wins!")
+elif turtB.xcor() > turtA.xcor():
+    print("Blue Wins!")
+elif turtB.xcor() == turtA.xcor():
+    print("It's a Tie!")
+time.sleep(3)
+turtA.clear()
+turtB.clear()
+turtA.up()
+turtB.up()
+turtA.left(180)
+turtB.left(180)
+turtA.goto(-100, 20)
+turtB.goto(-100, -20)
+turtA.left(180)
+turtB.left(180)
+race1 = False
+race2 = True
+print("Moving on the Race 2")
+turtA.down()
+turtB.down()
+for i in range(4):
+    if i == 3:
+        print("Go!")
+    else:
+        print(3 - i)
+        time.sleep(1)
+while race2 == True:
+    turtA.forward(random.randrange(1, 10))
+    time.sleep(0.075)
+    if turtA.xcor() >= 90 or turtB.xcor() >= 90:
+        race2 = False
+        break
+    turtB.forward(random.randrange(1, 10))
+    time.sleep(0.2)
+    if turtA.xcor() >= 90 or turtB.xcor() >= 90:
+        race2 = False
+        break
+    turtB.forward(random.randrange(1, 10))
+    time.sleep(0.075)
+    if turtA.xcor() >= 90 or turtB.xcor() >= 90:
+        race2 = False
+        break
+    turtA.forward(random.randrange(1, 10))
+    time.sleep(0.2)
+    if turtA.xcor() >= 90 or turtB.xcor() >= 90:
+        race2 = False
+        break
+if turtA.xcor() > turtB.xcor():
+    print("Red wins!")
+elif turtB.xcor() > turtA.xcor():
+    print("Blue Wins!")
+elif turtB.xcor() == turtA.xcor():
+    print("It's a Tie!")
+time.sleep(1)
+print("That concludes our Races!")
+time.sleep(3)
+turtA.clear()
+turtB.clear()
+turtA.up()
+turtB.up()
+turtA.left(180)
+turtB.left(180)
+turtA.goto(-100, 20)
+turtB.goto(-100, -20)
+turtA.left(180)
+turtB.left(180)
 
+print("Click to Continue to Part B")
+window.exitonclick()
 
 # PART B - complete part B here
+pygame.init()
+pyWindow = pygame.display.set_mode()
 
+numSides = [3, 4, 6, 9, 360]
+sideLength = 100
+offset = 100
 
-window.exitonclick()
+for n in numSides:
+    pyWindow.fill("black")
+    coords = [
+        0,
+    ] * n
+
+    for i in range(n):
+
+        theta = (2 * math.pi * (i)) / n
+        x = sideLength * math.cos(theta) + offset
+        y = sideLength * math.sin(theta) + offset
+        coords[i] = (x, y)
+    pygame.draw.polygon(pyWindow, "red", coords)
+    pygame.display.flip()
+    pygame.time.wait(2000)
