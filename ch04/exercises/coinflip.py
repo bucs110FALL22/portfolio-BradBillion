950185c310f30e44d6a1c42e00e12e426f0a0cb7
Bradley Vaval <90658099+BradBillion@users.noreply.github.com>
2022-10-03 22:26:41 +0000

    coinflip

---
+++ ch04/exercises/coinflip.py
@@ -0,0 +1,33 @@
+import turtle
+import random
+import time
+
+window = turtle.Screen()  # 2.  Create a screen
+window.setup(300, 300)
+window.bgcolor('lightblue')
+
+turt = turtle.Turtle()  # 3.  Create two turtles
+
+running = True
+while running is True:
+    print(turt.xcor(), turt.ycor())
+    direction = random.randrange(1, 3)
+    if direction == 1:
+        print("Heads")
+        turt.left(90)
+    elif direction == 2:
+        print("Tails")
+        turt.left(-90)
+    turt.forward(50)
+    print(turt.xcor(), turt.ycor())
+    if turt.xcor() >= 150 or turt.xcor() <= -150:
+        print("Out of Bounds")
+        running = False
+    elif turt.ycor() >= 150 or turt.ycor() <= -150:
+        print("Out of Bounds")
+        running = False
+    else:
+        print("Safe")
+    time.sleep(1)
+
+window.exitonclick()
